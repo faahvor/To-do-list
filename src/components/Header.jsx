fe6064e5e4cc1c7f3a0d2c7d7d13ad5159ba377d
faahvor <tinafavour2018@gmail.com>
2024-08-08 11:48:20 +0100

    todo

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { FaRegMoon } from "react-icons/fa";
+
+
+
+const Header=()=>{
+    return(
+        <div className="flex justify-between px-5 py-4 items-center w-full bg-red-500">
+          <h1 className="font-semibold text-2xl">TODO</h1>
+          <FaRegMoon />
+
+        </div>
+    )
+}
+export default Header
